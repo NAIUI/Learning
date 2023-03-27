3ee881e1d83d1dd1b0eae81f15e5500a86475822
fan forever <2743357832@qq.com>
2023-03-27 13:12:26 +0800

    add IProgress

--- Observer Pattern/FileSplitter.cpp
+++ Observer Pattern/FileSplitter.cpp
@@ -1,12 +1,22 @@
+class IProgress {
+public:
+	virtual void DoProgress(float value) = 0;
+	virtual ~IProgress(){}
+};
+
 class FileSplitter
 {
 	string m_filePath;
 	int m_fileNumber;
 
+	//ProgressBar* m_progressBar;	//具体通知控件
+	IProgress* m_iprogress;			//抽象通知机制
+
 public:
 	FileSplitter(const string& filePath, int fileNumber) :
 		m_filePath(filePath),
-		m_fileNumber(fileNumber) {
+		m_fileNumber(fileNumber),
+		m_iprogress(iprogress) {
 
 	}
 
@@ -18,6 +28,11 @@ public:
 		for (int i = 0; i < m_fileNumber; ++i) {
 			//...
 
+			if (m_iprogress != null) {
+				float progressValue = m_fileNumber;
+				progressValue = (i + 1) / progressValue;
+				m_iprogress->DoProgress(progressValue);	//更新进度条
+			}
 		}
 	}
 };
--- Observer Pattern/MainForm.cpp
+++ Observer Pattern/MainForm.cpp
@@ -3,13 +3,15 @@ class Mainform : public Form
     TextBox* txtFilePath;
     TextBox* txtFileNumber;
 
+    ProgressBar* progressBar;
+
 public:
     void Button1_Click() {
 
         String filePath = txtFilePath->getText();
         int number = atoi(txtFileNumber->getText().c_str());
 
-        FileSplitter splitter(filePath, number);
+        FileSplitter splitter(filePath, number, progressBar);
 
         splitter.split();
 
