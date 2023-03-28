7ca6bcc17c2f39ef72796d169f08c4673508b034
fan forever <2743357832@qq.com>
2023-03-28 21:22:08 +0800

    add factory pattern

---
+++ ”对象创建“模式/Factory Method/MainForm2.cpp
@@ -0,0 +1,19 @@
+class MainForm : public Form
+{
+	SplitterFactory* factory;
+
+public:
+	MainForm(SplitterFactory* factory) {
+		this->factory = factory;
+	}
+	void Button1_Click() {
+
+
+
+		ISplitter* splitter =
+			factory->CreateSplitter();	//¶àÌ¬new
+
+		splitter->split();
+
+	}
+};
