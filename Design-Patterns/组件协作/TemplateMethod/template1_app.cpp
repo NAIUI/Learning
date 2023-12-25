82457274b15520c9f7725763ec2ad2d893d273a7
fan forever <2743357832@qq.com>
2023-12-25 13:18:11 +0800

    Add TemplateMethod

---
+++ Design-Patterns/组件协作/TemplateMethod/template1_app.cpp
@@ -0,0 +1,34 @@
+// 应用程序开发人员
+class Application
+{
+public:
+	bool Step2()
+	{
+		//...
+	}
+
+	void Step4()
+	{
+		//...
+	}
+};
+
+int main()
+{
+	Library lib();
+	Application app();
+
+	lib.Step1();
+
+	if (app.Step2())
+	{
+		lib.Step3();
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		app.Step4();
+	}
+
+	lib.Step5();
+}
